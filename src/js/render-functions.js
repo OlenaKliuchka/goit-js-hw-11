e3e510495f3f90efc9b22c666b10f660237232ca
ElenaKliuchka <klychikel@gmail.com>
2024-05-10 14:51:59 +0300

    css

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -10,20 +10,34 @@ export const createImageGalleryItem = images => {
         downloads,
         comments,
       }) => `
-      <div class="col">
-        <div class="card shadow-sm">
-        <a class="gallery-link" href="${largeImageURL}">
-          <img src="${webformatURL}" alt="${tags}" class="gallery-img img-fluid" />
-          <div class="card-body">
-            <div class="d-flex justify-content-between align-items-center">
-              <small class="text-body-secondary">Likes: ${likes}</small>
-              <small class="text-body-thirdary">vVews: ${views}</small>
-              <small class="text-body-fourthary">Comments: ${comments}</small>
-              <small class="text-body-fifthary">Downloads: ${downloads}</small>
+  
+  <li class="gallery-item">
+            <a class="gallery-link" href="${largeImageURL}">
+              <img
+                class="gallery-image"
+                src="${webformatURL}"
+                alt="${tags}"></a>
+            
+            <div class = "card-img">
+            <div class = "likes">
+            <h2 class = "card-title">Likes</h2>
+            <p class = image-text>${likes}</p></div>
+            
+            <div class = "views">
+            <h2 class = "card-title">Views</h2>
+            <p class = image-text>${views}</p></div>
+
+            <div class = "comments">
+            <h2 class = "card-title">Comments</h2>
+            <p class = image-text>${comments}</p></div>
+            
+            <div class = "downloads">
+            <h2 class = "card-title">Downloads</h2>
+            <p class = image-text>${downloads}</p></div>
             </div>
-          </div>
-        </div>
-      </div>`
+            
+            
+          </ >`
     )
     .join('');
 };
